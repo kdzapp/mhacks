053c386c63d54c5e06c5d29496d6379de192353a
kdzapp <kdzapp@umich.edu>
2016-02-17 09:23:29 -0500

    Initial Commit

---
+++ spot/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  spot
+//
+//  Created by Kyle Zappitell on 2/17/16.
+//  Copyright © 2016 Kyle Zappitell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
