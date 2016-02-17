eb26f74fa805e8fed013e36bb29c30f020c727bf
kdzapp <kdzapp@umich.edu>
2016-02-17 11:01:11 -0500

    Added file for Projectile Class

---
+++ spot/ProjectileMod.swift
@@ -0,0 +1,14 @@
+//
+//  ProjectileMod.swift
+//  spot
+//
+//  Created by Kyle Zappitell on 2/17/16.
+//  Copyright © 2016 Kyle Zappitell. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+
+class ProjectileMod {
+    //Class of a Projectile
+}
