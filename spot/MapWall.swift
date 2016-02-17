bd98c4b8cfe1c2e249bc5243adc03dcc62120c32
kdzapp <kdzapp@umich.edu>
2016-02-17 09:58:51 -0500

    Added Map Wall File for Orginization

---
+++ spot/MapWall.swift
@@ -0,0 +1,13 @@
+//
+//  MapWall.swift
+//  spot
+//
+//  Created by Kyle Zappitell on 2/17/16.
+//  Copyright © 2016 Kyle Zappitell. All rights reserved.
+//
+
+import Foundation
+
+class MapWall {
+    //Map Walls will all be instinces of this class
+}
