f2d68ddbf060697f911e8d0dac8fe5182928b640
Mike Wiitala <mzwiital@umich.edu>
2016-02-20 23:14:53 -0500

    Added 3rd Map w color change function

--- spot/GameScene.swift
+++ spot/GameScene.swift
@@ -14,7 +14,8 @@ class GameScene: SKScene {
     let joystick = AnalogJoystick(diameters: (substrate: 130, stick: 70), colors: (substrate: UIColor(red: 0.2, green: 0.2, blue: 0.2, alpha: 0.7), stick: UIColor(red: 0.4, green: 0.4, blue: 0.4, alpha: 0.5)), images: (nil))
     let player = Sprite(color: spriteColor.green)
     let enemy = Sprite(color: spriteColor.red)
-    let gameBackgroundColor = UIColor(red: 0.26, green: 0.26, blue: 0.26, alpha: 1);
+    let gameBackgroundColor = UIColor(red: 0.26, green: 0.26, blue: 0.26, alpha: 1)
+
     
     override func didMoveToView(view: SKView) {
         
@@ -40,7 +41,7 @@ class GameScene: SKScene {
         joystick.position = CGPoint(x: 200, y: 265)
         
         //Create Map
-        self.addChild(createMap())
+        self.addChild(createMap3())
         self.addChild(joystick)
     }
     
@@ -114,3 +115,77 @@ func createMap() -> SKNode {
     
     return viewer
 }
+
+func createMap2() -> SKNode {
+    let viewer = SKNode()
+    
+    let wallSqr0 = Wall(width: 150, height: 150, xCord: 200, yCord: 510)
+    viewer.addChild(wallSqr0)
+    let wallSqr1 = Wall(width: 150, height: 150, xCord: 500, yCord: 510)
+    viewer.addChild(wallSqr1)
+    let wallSqr2 = Wall(width: 150, height: 150, xCord: 800, yCord: 510)
+    viewer.addChild(wallSqr2)
+    let wallSqr3 = Wall(width: 150, height: 150, xCord: 200, yCord: 260)
+    viewer.addChild(wallSqr3)
+    let wallSqr4 = Wall(width: 150, height: 150, xCord: 500, yCord: 260)
+    viewer.addChild(wallSqr4)
+    let wallSqr5 = Wall(width: 150, height: 150, xCord: 800, yCord: 260)
+    viewer.addChild(wallSqr5)
+    let wallTopBorder = Wall(width: 1100, height: 10, xCord: 500, yCord: 679)
+    viewer.addChild(wallTopBorder)
+    let wallBottomBorder = Wall(width: 1100, height: 10, xCord: 500, yCord: 89)
+    viewer.addChild(wallBottomBorder)
+    
+    return viewer
+}
+
+func createMap3() -> SKNode {
+    let viewer = SKNode()
+    let angle: CGFloat = CGFloat(M_PI)
+    let rotate = SKAction.rotateByAngle(angle, duration: 2)
+    let repeateAction = SKAction.repeatActionForever(rotate)
+    
+    let wallTopLeft = Wall(width: 180, height: 20, xCord: 205, yCord: 520)
+    wallTopLeft.zRotation = 0.78
+    viewer.addChild(wallTopLeft)
+    let wallBottomLeft = Wall(width: 180, height: 20, xCord: 205, yCord: 245)
+    wallBottomLeft.zRotation = -0.78
+    viewer.addChild(wallBottomLeft)
+    let wallTopRight = Wall(width: 180, height: 20, xCord: 800, yCord: 520)
+    wallTopRight.zRotation = -0.78
+    viewer.addChild(wallTopRight)
+    let wallBottomRight = Wall(width: 180, height: 20, xCord: 800, yCord: 245)
+    wallBottomRight.zRotation = 0.78
+    viewer.addChild(wallBottomRight)
+    let wallSqr = Wall(width: 120, height: 120, xCord: 500, yCord: 380)
+    wallSqr.runAction(repeateAction, withKey: "rotate")
+    viewer.addChild(wallSqr)
+    let wallTopBorder = Wall(width: 1100, height: 10, xCord: 500, yCord: 679)
+    viewer.addChild(wallTopBorder)
+    let wallBottomBorder = Wall(width: 1100, height: 10, xCord: 500, yCord: 89)
+    viewer.addChild(wallBottomBorder)
+    
+    let changeColorAction = SKAction.colorizeWithColor(UIColor.redColor(), colorBlendFactor: 0.08, duration: 1.5)
+    wallSqr.runAction(changeColorAction) {
+        wallSqr.wallColor = UIColor.redColor()
+    }
+    
+    return viewer
+}
+
+/*func createMap4() -> SKNode {
+    let viewer = SKNode()
+    let distance: CGVector = CGVectorMake(0, 100)
+    let reverse: CGVector = CGVectorMake(0, -100)
+    let translate = SKAction.moveBy(distance, duration: 1.5)
+    let translateBack = SKAction.moveBy(reverse, duration: 1.5)
+    let repeateAction = SKAction.repeatAction(translate, count: 400)
+    let repeatActionBack = SKAction.repeatAction(translateBack, count: 400)
+    
+    let wallSqr = Wall(width: 120, height: 120, xCord: 500, yCord: 380)
+    wallSqr.runAction(repeateAction, withKey: "translate")
+    wallSqr.runAction(repeatActionBack, withKey: "reverse")
+    viewer.addChild(wallSqr)
+    return viewer
+}*/
+
