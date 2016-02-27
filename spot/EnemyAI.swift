eaed5a560acda67428e23560349b0e8827b38f97
Mike Wiitala <mzwiital@umich.edu>
2016-02-26 22:26:37 -0500

    EnemyAI

---
+++ spot/EnemyAI.swift
@@ -0,0 +1,56 @@
+//
+//  EnemyAI.swift
+//  spot
+//
+//  Created by Mike Wiitala on 2/26/16.
+//  Copyright © 2016 Kyle Zappitell. All rights reserved.
+//
+
+import Foundation
+import SpriteKit
+import UIKit
+
+func moveTowardPlayer(player: Sprite, enemy: Sprite) -> CGPoint {
+    var moveEnemy: CGPoint
+    moveEnemy = CGPointMake(0, 0)
+    moveEnemy.y = (enemy.position.y + ((player.position.y - enemy.position.y) / 100 ))
+    moveEnemy.x = (enemy.position.x + ((player.position.x - enemy.position.x) / 100 ))
+    
+    return moveEnemy
+    
+}
+
+
+
+func moveAwayFromPlayer(player: Sprite, enemy: Sprite) -> CGPoint {
+    var moveEnemy: CGPoint
+    moveEnemy = CGPointMake(0, 0)
+    moveEnemy.y = (enemy.position.y - ((player.position.y - enemy.position.y) / 100 ))
+    moveEnemy.x = (enemy.position.x - ((player.position.x - enemy.position.x) / 100 ))
+    
+    return moveEnemy
+    
+}
+
+func enemyProjectile(player: Sprite, enemy: Sprite) -> Projectile {
+    let projectile = Projectile(sprite: enemy)
+    var location: CGPoint
+    location = player.position
+    projectile.position = enemy.position
+    projectile.setVelocity((location.y - projectile.position.y), dx: (location.x-projectile.position.x))
+    
+    return projectile
+    
+}
+
+/*func clearLineOfSight(player: Sprite, enemy: Sprite) -> Bool {
+let hieght = Double(player.position.x - enemy.position.x)
+let width = Double(player.position.y - enemy.position.y)
+let hypotenuse = CGFloat(sqrt(pow(hieght, 2) + pow(width, 2)))
+let lineOfSight = SKSpriteNode(color: UIColor.clearColor(), size: CGSizeMake(3, hypotenuse))
+return false
+}*/
+//REMOVE
+//self.enemy.position = moveTowardPlayer(self.player, enemy: self.enemy)
+//self.addChild(enemyProjectile(self.player, enemy: self.enemy))
+
